63c14429c7f3349a06038cee957977c9c841ea10
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-09-21 18:49:36 +0530

    implemented google auth

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,14 +1,55 @@
 import "../HomePage/Home.scss"
 import { Link } from "react-router-dom"
+import jwtDecode from "jwt-decode"
+import { useEffect, useState } from "react"
 import Homepagebox from "../../Components/HomePageBox/HomePageBox"
-const Home =()=>{
-return(
-    <div className="Homewrapper">
-        <Link className="link" to="/ExplorePath"><Homepagebox title="Explore Paths" logo="https://career-development-bits-pilani.notion.site/icons/search_blue.svg?mode=dark"/></Link>
-        <Link className="link" to="/Upskill"><Homepagebox title="Up-Skill" logo="https://career-development-bits-pilani.notion.site/icons/hammer_blue.svg?mode=dark"/></Link>
-        <Link className="link" to="/CrackInterviews"><Homepagebox logo="https://career-development-bits-pilani.notion.site/icons/friends_blue.svg?mode=dark" title="Crack Interviews"/></Link>
+const Home = () => {
+    const [user, setUser] = useState({});
+    function handleCallbackResponse(response) {
 
-    </div>
-)
+
+        var userObject = jwtDecode(response.credential);
+        console.log(userObject);
+        setUser(userObject);
+        document.getElementById("signInDiv").hidden = true;
+    }
+    function handleSignOut(event) {
+        setUser({});
+        document.getElementById("signInDiv").hidden = false;
+
+    }
+
+    useEffect(() => {
+        google.accounts.id.initialize({
+            client_id: "1028850909564-e2hggnfb2m3n80vbtitdnf54d2j09gsi.apps.googleusercontent.com",
+            callback: handleCallbackResponse
+        });
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"), { theme: "outline", size: "large" }
+        );
+        {Object.keys(user).length == 0 && google.accounts.id.prompt()}
+    })
+
+    return (
+        <div className="signin">
+            <div id="signInDiv"></div>
+
+            {Object.keys(user).length != 0 &&
+            <div className="allwrap">
+                <div className="Homewrapper">
+                    <Link className="link" to="/ExplorePath"><Homepagebox title="Explore Paths" logo="https://career-development-bits-pilani.notion.site/icons/search_blue.svg?mode=dark" /></Link>
+                    <Link className="link" to="/Upskill"><Homepagebox title="Up-Skill" logo="https://career-development-bits-pilani.notion.site/icons/hammer_blue.svg?mode=dark" /></Link>
+                    <Link className="link" to="/CrackInterviews"><Homepagebox logo="https://career-development-bits-pilani.notion.site/icons/friends_blue.svg?mode=dark" title="Crack Interviews" /></Link>
+                </div>
+                <div className="signout">
+                <button className="signOutBtn" onClick={(e) => handleSignOut(e)}>Sign Out</button>
+                </div>
+
+                </div>
+            }
+        </div>
+
+
+    );
 }
 export default Home
