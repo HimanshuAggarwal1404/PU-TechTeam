2784949247f0d9d57610ee15fdb04a712e9b9d79
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-09-26 06:01:18 +0530

    Update CrackInterviews.jsx

--- src/Pages/CrackInterviewsPage/CrackInterviews.jsx
+++ src/Pages/CrackInterviewsPage/CrackInterviews.jsx
@@ -4,7 +4,7 @@ const CrackInterviews = () => {
     return (
         <div className="CrackInterviewWrapper">
             <Block url="https://images.unsplash.com/photo-1594904351111-a072f80b1a71?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&w=1200" text="IT"/>
-            <Block url="https://file.notion.so/f/s/8c9b7f15-b41d-4079-a96f-495c81750b78/e244f49c-cb29-4bab-9d5b-6d5d03de784f_product-launch-feature.avif?id=da29165f-7169-4d12-ab4c-1fdfc61e542a&table=block&spaceId=add8950f-c935-46b2-861e-df4f67053f75&expirationTimestamp=1694916000000&signature=iZ0mFhYXrYlp6gJIJXsmgwN8RgSkdW9I5rBXxdSrQAc" text="Product Management"/>
+            <Block url="https://file.notion.so/f/s/8c9b7f15-b41d-4079-a96f-495c81750b78/e244f49c-cb29-4bab-9d5b-6d5d03de784f_product-launch-feature.avif?id=da29165f-7169-4d12-ab4c-1fdfc61e542a&table=block&spaceId=add8950f-c935-46b2-861e-df4f67053f75&expirationTimestamp=1695780000000&signature=vL1A9E7as8EBe80gXsY31YaUmtis4wJ56ySe_Y1OWoo" text="Product Management"/>
             <Block url="https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1200" text="ML/Data Analytics"/>
             <Block url="https://images.unsplash.com/photo-1450101499163-c8848c66ca85?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1200" text="Case Interview"/>
             <Block url="https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixlib=rb-4.0.3&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1200" text="Quant"/>
