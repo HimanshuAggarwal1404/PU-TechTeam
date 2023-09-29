33700baa0841e962ab05e52e76aa1abade624652
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-09-30 01:55:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './Components/Header/Header'
+// import Header from './Components/Header/Header'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/HomePage/Home'
 import ExplorePath from './Pages/ExplorePathPage/ExplorePath';
