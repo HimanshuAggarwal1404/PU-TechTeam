957fe6f8ae6ae7be23eed9bfa4ad5c6e7f796114
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-09-30 06:28:10 +0530

    -

--- src/Pages/Explore Path Pages/CompetetiveCoding/CompetitiveCoding.jsx
+++ src/Pages/Explore Path Pages/CompetetiveCoding/CompetitiveCoding.jsx
@@ -6,7 +6,14 @@ const CompetitiveCoding = () => {
     document.title= 'Explore Path: Competitive Coding';
 },[]);
   return (
-    <div>CompetitiveCoding</div>
+    <div className={styles.ccwrapper}>
+      <div className={styles.introduction}>
+        <div className={styles.introductionTitle}>INTRODUCTION</div>
+        <div className={styles.introductionContent}>Once you enter programming you just don’t learn how to code but you also know the “art of thinking”, by breaking your code into smaller chunks and then using your logic-based creativity to solve a problem from different angles. Programming is a fun exercise for your brain, programming is a mental sport. When this sport is held on the internet involving various sports programmers as contestants, then it is called Competitive Programming. In this, participants compete to solve programming problems using algorithms and data structures within a set time limit. The competitions are usually held online or onsite, and the challenges are designed to test the participants' problem-solving skills, efficiency, and accuracy.
+<br />
+Competitive coding involves solving problems in various categories such as data structures, algorithms, dynamic programming, graph theory, and more.</div>
+      </div>
+    </div>
   )
 }
 
